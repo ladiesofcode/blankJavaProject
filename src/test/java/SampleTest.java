6d337153c49efef380c729e0f0898c1b4adbab07
Sarah Usher <sarahnusher@gmail.com>
2020-10-14 17:11:08 +0100

    maven set up

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SampleTest {
+
+    @org.junit.jupiter.api.Test
+    public void checkFailingTest(){
+        assertEquals(1, 2);
+    }
+}
